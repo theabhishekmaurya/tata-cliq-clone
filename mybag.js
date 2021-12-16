f6d53eee64903d4ddf1504f8d80863f9bb51493e
anchor-abhi <theabhishek1802@gmail.com>
2021-12-17 00:09:05 +0530

    updated

--- mybag.js
+++ mybag.js
@@ -1,3 +1,12 @@
 document.querySelector("#my_account").addEventListener("click",function(){
     window.location.href="myaccount.html";
 })
+var products=JSON.parse(localStorage.getItem("cart_history")) || [];
+var item=document.querySelector("#itemcontainer");
+
+products.map(function(){
+    var itemClone=item.cloneNode(true);
+    itemClone.style.display="flex"
+    itemClone.style.position="relative"
+    document.querySelector("#itemdiv").append(itemClone)
+})
