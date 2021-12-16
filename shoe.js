ff80cec22e565ed42af7d10cdf41d6e0da682705
Tarasish bhattacharya <tarasishbhattacharya06@gmail.com>
2021-12-16 11:08:03 +0530

    update

---
+++ shoe.js
@@ -0,0 +1,133 @@
+var data=[{
+    img:"https://img.tatacliq.com/images/i2/437Wx649H/MP000000002056178_437Wx649H_20171125205630.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "3.5⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i7/437Wx649H/MP000000010478798_437Wx649H_202109021215471.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "4⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i2/437Wx649H/MP000000002056178_437Wx649H_20171125205630.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "4⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i7/437Wx649H/MP000000008552928_437Wx649H_202101132212361.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "3.5⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i2/437Wx649H/MP000000002056178_437Wx649H_20171125205630.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "4⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i7/437Wx649H/MP000000008552928_437Wx649H_202101132212361.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "4⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i7/437Wx649H/MP000000010478798_437Wx649H_202109021215471.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "4⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i7/437Wx649H/MP000000008552928_437Wx649H_202101132212361.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "4⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i7/437Wx649H/MP000000010478798_437Wx649H_202109021215471.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "3.5⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i7/437Wx649H/MP000000008552928_437Wx649H_202101132212361.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "5⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i7/437Wx649H/MP000000010478798_437Wx649H_202109021215471.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "5⭐",
+},
+{
+    img:"https://img.tatacliq.com/images/i7/437Wx649H/MP000000008552928_437Wx649H_202101132212361.jpeg",
+    brand:"Red chief",
+    title:"Red chief casual shoe",
+    product:"shoe",
+    Newprice:" Rs 2249",
+    oldPrice:"4499",
+    button: "5⭐",
+},
+
+
+]
+display(data);
+function display(){
+    data.map(function(elem){
+        var div=document.createElement("div");
+        var img=document.createElement("img");
+        img.setAttribute("src",elem.img);
+        var para=document.createElement("p");
+        para.textContent=elem.brand
+        var para2=document.createElement("p");
+        para2.textContent=elem.title
+        var para3=document.createElement("p");
+        para3.textContent=elem.product
+        var span=document.createElement("span");
+        span.textContent=elem.Newprice
+        var span2=document.createElement("span");
+        span2.textContent=elem.oldPrice;
+        var btn=document.createElement("button");
+        btn.textContent=elem.button;
+        div.append(img,para,para2,para3,span,span2,btn);
+        document.querySelector(".right_box").append(div);
+    })
+}
