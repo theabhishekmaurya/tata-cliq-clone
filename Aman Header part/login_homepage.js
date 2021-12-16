ffe1f27e6235ed09280a777370356a9a29e527c7
aman-638 <ar7009297@gmail.com>
2021-12-16 13:42:50 +0530

    js changed updated

---
+++ Aman Header part/login_homepage.js
@@ -0,0 +1,41 @@
+document.querySelector("#categories").addEventListener("mouseover",function(){
+    document.querySelector("#category").style.display="flex";
+})
+
+document.querySelector("#category").addEventListener("mouseover",function(){
+    document.querySelector("#category").style.display="flex";
+})
+document.querySelector("#category").addEventListener("mouseout",function(){
+    document.querySelector("#category").style.display="none";
+})
+document.querySelector("#categories").addEventListener("mouseout",function(){
+    document.querySelector("#category").style.display="none";
+})
+
+document.querySelector("#brands").addEventListener("mouseover",function(){
+    document.querySelector("#brand").style.display="flex";
+})
+
+document.querySelector("#brand").addEventListener("mouseover",function(){
+    document.querySelector("#brand").style.display="flex";
+})
+document.querySelector("#brand").addEventListener("mouseout",function(){
+    document.querySelector("#brand").style.display="none";
+})
+document.querySelector("#brands").addEventListener("mouseout",function(){
+    document.querySelector("#brand").style.display="none";
+})
+
+
+document.querySelector("#signin_up").addEventListener("mouseover", function(){
+    document.querySelector(".signup").style.display="grid"
+})
+document.querySelector("#signin_up").addEventListener("mouseout", function(){
+    document.querySelector(".signup").style.display="none"
+})
+document.querySelector(".signup").addEventListener("mouseover", function(){
+    document.querySelector(".signup").style.display="grid"
+})
+document.querySelector(".signup").addEventListener("mouseout", function(){
+    document.querySelector(".signup").style.display="none"
+})
